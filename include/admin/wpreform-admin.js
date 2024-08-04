8fa19bf5eb9bd2bd981c0ebe47ad2061b941f119
PapierCode <julien@papier-code.fr>
2024-08-04 18:00:19 +0200

    3.0.89 Compteur texte, markdown

--- include/admin/wpreform-admin.js
+++ include/admin/wpreform-admin.js
@@ -7,21 +7,31 @@ document.addEventListener( 'DOMContentLoaded', function () {
     
     if ( pcCounters.length > 0 ) {
 
-        const updatePcCounter = ( counter, max, length, value ) =>   {          
-            value.textContent = length;
+        const updatePcCounter = ( counter, max, txt, display ) => {   
+            txt = txt.replace(/\[{1,}([ a-z0-9]+)\]{1,}/img, '$1');
+            txt = txt.replace(/\{{1,}([ a-z0-9]+)\}{1,}/img, '$1');
+            txt = txt.replace(/\{|\}|\[|\]/g, '');                                   
+            display.textContent = txt.length;
             if ( length > max ) { counter.style.color = 'red'; }
             else { counter.removeAttribute('style'); }
         };
 
         pcCounters.forEach( (counter) => {
             const max = counter.dataset.size;
-            const value = counter.querySelector('.pc-txt-length-value');
+            const display = counter.querySelector('.pc-txt-length-value');
             const field = counter.closest('.acf-input').querySelector('input,textarea');
-            updatePcCounter( counter, max, field.value.length, value );
+            updatePcCounter( counter, max, field.value, display );
             field.addEventListener( 'input', (event) => { 
-                updatePcCounter( counter, max, event.currentTarget.value.length, value );
+                updatePcCounter( counter, max, event.currentTarget.value, display );
             } );
         });
+
+        // acf.add_action('wysiwyg_tinymce_init', function( ed, id, mceInit, field ){
+        //     const counter = field[0].querySelector('.pc-txt-length-counter');
+        //     const value = counter.querySelector('.pc-txt-length-value');
+        //     updatePcCounter( counter, 300, ed.getBody().textContent.length, value );
+        //     ed.on('change', (event)=> { updatePcCounter( counter, 300, ed.getBody().textContent.length, value ); });
+        // });
 
     }
 
